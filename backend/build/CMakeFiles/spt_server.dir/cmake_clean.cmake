573916cddfc202b76bc43295f650f601812266e1
Shardul Vikram Singh <shardulvikrm@gmail.com>
2024-10-05 04:00:21 +0530

    Initial commit

---
+++ backend/build/CMakeFiles/spt_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spt_server.dir/main.cpp.o"
+  "CMakeFiles/spt_server.dir/main.cpp.o.d"
+  "spt_server"
+  "spt_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spt_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
